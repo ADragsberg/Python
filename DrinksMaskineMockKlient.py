88c99d1693c8f35a54af8fefb078b21a8ebc3ca4
TrisES <112662675+TrisES@users.noreply.github.com>
2023-12-12 13:49:01 +0100

    Simpel "virkende PI" udgave.

--- DrinksMaskineMockKlient.py
+++ DrinksMaskineMockKlient.py
@@ -11,6 +11,33 @@ START_CUE = "start"
 
 tcpCliSock = socket(AF_INET, SOCK_STREAM)
 
+# Lav Drink Metode (Antager at data er en json string med antal ingredienser samt en dict med ingredienser og mængder)
+# Skal kigge på opskrift og bede bruger om at at "fylde glas op" med ingredienserne via pile op og ned
+def lavDrink(data):
+    print("Du skal nu lave en drink med følgende ingredienser:")
+    print(data)
+
+    keys = list(data['ingredienser'].keys())
+    values = list(data['ingredienser'].values())
+    print(keys)
+    print(values)
+
+    i = 0
+    while i < data['antalIngs']:
+        print(f"Fyld {values[i]} ml {keys[i]} i glasset")
+
+        amountToFill = int(values[i])
+        actualAmount = 0
+
+        while actualAmount < amountToFill:
+            # Hvis bruger trykker på pil op
+            if input().lower() == "up":
+                actualAmount += 50
+                print(f"{actualAmount} ml {keys[i]} i glasset")
+
+        i +=1
+    
+
 # Læs input fra bruger
 # Når input er "start" så sendes forespørgslen til serveren, som svarer med en drinkopskrift
 while True:
@@ -29,8 +56,9 @@ while True:
         break
     # json load
     data = json.loads(data)
-    print("Modtaget data fra serveren, printer data:")
-    print(data)
+    lavDrink(data)
+    # print("Modtaget data fra serveren, printer data:")
+    # print(data)
 
 print("Forbindelsen lukkes")
 tcpCliSock.close()
